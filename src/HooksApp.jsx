660f95265dd0bfe27e38add0e83077a55c1eb8b0
Gabriel Garcia <gabito1966@gmail.com>
2024-04-25 11:15:59 -0300

    first commit

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import { ContadorComponent } from "./components/ContadorComponent"
+
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Aplicacion de hooks</h1>
+      <hr />
+      <ContadorComponent></ContadorComponent>
+    </>
+  )
+}
+
